92ad280746b9a88929b769332a90e7da0899520f
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2024-11-26 11:52:03 -0300

    Add project files.

---
+++ Poc.Console.Example/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Poc.Console.Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
